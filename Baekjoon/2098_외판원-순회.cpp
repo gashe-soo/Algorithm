c094e0c13a8a1449b5db4e1243046a950901c0b8
dev-to-dev <gashe7@gmail.com>
2020-10-08 01:28:02 +0900

    update comment

--- Baekjoon/2098_외판원-순회.cpp
+++ Baekjoon/2098_외판원-순회.cpp
@@ -1,8 +1,8 @@
-// ¹éÁØ 2098¹ø ¿ÜÆÇ¿ø ¼øÈ¸
+// ë°±ì¤€ 2098ë²ˆ ì™¸íŒì› ìˆœíšŒ
 
-#include <iostream>
-#include <cstring>
 #include <algorithm>
+#include <cstring>
+#include <iostream>
 using namespace std;
 
 int n;
@@ -11,37 +11,42 @@ int dp[16][1 << 16];
 
 const int INF = 987654321;
 
-int solve(int cur, int v) {
-	if (v == (1 << n) - 1) {
-		if (map[cur][0] != 0) {
-			return map[cur][0];
-		}
-		return INF;
-	}
-	int& ret = dp[cur][v];
+int solve(int cur, int v)
+{
+    if (v == (1 << n) - 1) {
+        if (map[cur][0] != 0) {
+            return map[cur][0];
+        }
+        return INF;
+    }
+    int& ret = dp[cur][v];
 
-	if (ret != -1)
-		return ret;
+    if (ret != -1)
+        return ret;
 
-	ret = INF;
-	for (int i = 0; i < n; i++) {
-		if (v & (1 << i)) continue;
-		if (map[cur][i] == 0) continue;
-		ret = min(ret, solve(i, (v | (1 << i))) + map[cur][i]);
-	}
-	return ret;
-
+    ret = INF;
+    for (int i = 0; i < n; i++) {
+        if (v & (1 << i))
+            continue;
+        if (map[cur][i] == 0)
+            continue;
+        ret = min(ret, solve(i, (v | (1 << i))) + map[cur][i]);
+    }
+    return ret;
 }
 
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	cin >> n;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++)
-			cin >> map[i][j];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> map[i][j];
 
-	memset(dp, -1, sizeof(dp));
-	cout << solve(0, 1);
+    memset(dp, -1, sizeof(dp));
+    cout << solve(0, 1);
 
-	return 0;
+    return 0;
 }
