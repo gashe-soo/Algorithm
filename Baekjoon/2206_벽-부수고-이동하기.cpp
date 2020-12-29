728ff1e25eb31c3d336bcd700f12c1f1af664256
dev-to-dev <gashe7@gmail.com>
2020-12-29 21:25:12 +0900

    201229 BOJ retry

--- Baekjoon/2206_벽-부수고-이동하기.cpp
+++ Baekjoon/2206_벽-부수고-이동하기.cpp
@@ -1,61 +1,66 @@
-// 백준 2206번 벽 부수고 이동하기
+// 백준 2206번 벽-부수고-이동하기
 
 #include <iostream>
 #include <queue>
 #include <string>
+
 using namespace std;
 
+struct Node {
+    int x, y, c, d;
+};
+
 int n, m;
-int map[1001][1001];
-int v[1001][1001][2];
+int wall[1001][1001];
+bool v[1001][1001][2];
+int dx[4] = { -1, 1, 0, 0 }, dy[4] = { 0, 0, -1, 1 };
+string in;
 
-int dx[4] = { -1,1,0,0 };
-int dy[4] = { 0,0,-1,1 };
+int bfs()
+{
+    queue<Node> q;
+    q.push({ 0, 0, 1, 1 });
+    v[0][0][1] = 1;
 
-int bfs() {
-	queue<vector<int>> q;
-	q.push({ 0,0,1 });
-	v[0][0][1] = 1;
-	
-	while (!q.empty()) {
-		vector<int> arr = q.front();
-		int x = arr[0], y = arr[1], cnt = arr[2];
-		q.pop();
-		
-		if (x == n - 1 && y == m - 1)
-			return v[x][y][cnt];
+    while (!q.empty()) {
+        Node node = q.front();
+        q.pop();
+        if (node.x == n - 1 && node.y == m - 1) {
+            return node.d;
+        }
 
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i], ny = y + dy[i];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) 
-				continue;
-			if (cnt == 0 && map[nx][ny] == 1)
-				continue;
-			if (cnt && map[nx][ny] == 1) {
-				q.push({ nx,ny,0 });
-				v[nx][ny][0] = v[x][y][cnt] + 1;
-			}
-			else if (v[nx][ny][cnt] ==0 && map[nx][ny] == 0) {
-				q.push({ nx,ny,cnt });
-				v[nx][ny][cnt] = v[x][y][cnt] + 1;
-			}
-		}
-	}
-	return -1;
+        for (int i = 0; i < 4; i++) {
+            int nx = node.x + dx[i], ny = node.y + dy[i];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+                continue;
+
+            if (wall[nx][ny] == 0 && !v[nx][ny][node.c]) {
+                v[nx][ny][node.c] = 1;
+                q.push({ nx, ny, node.c, node.d + 1 });
+            }
+
+            if (wall[nx][ny] == 1 && node.c == 1) {
+                v[nx][ny][1] = 1;
+                q.push({ nx, ny, 0, node.d + 1 });
+            }
+        }
+    }
+    return -1;
 }
 
-int main() {
-	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-	cin >> n >> m;
-	
-	string s;
-	for (int i = 0; i < n; i++) {
-		cin >> s;
-		for (int j = 0; j < m; j++) {
-			map[i][j] = s[j] - '0';
-		}
-	}
-	cout << bfs();
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
 
-	return 0;
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> in;
+        for (int j = 0; j < m; j++)
+            wall[i][j] = in[j] - '0';
+    }
+
+    cout << bfs();
+    return 0;
 }
