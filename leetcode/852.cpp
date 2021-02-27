2a152ee95d959b8790841015a01830e397891b3a
dev-to-dev <gashe7@gmail.com>
2021-02-27 22:05:08 +0900

    210227 LC

---
+++ leetcode/852.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr)
+    {
+        int l = 0, r = arr.size() - 1;
+        int mid;
+
+        while (l < r) {
+            mid = (l + r) / 2;
+            if (arr[mid] < arr[mid + 1]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return l;
+    }
+};
