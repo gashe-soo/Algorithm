cbf68f43695cd0d7031c78bf83c30222b5c77e3a
gashe-soo <gashe7@gmail.com>
2021-05-09 22:34:03 +0900

    210509 PRO

---
+++ Programmers/level2/level2_JadenCase-문자열-만들기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] sp = s.toLowerCase().split("");
+        boolean flag = true;
+
+        for (String ss : sp) {
+            answer += flag ? ss.toUpperCase() : ss;
+            flag = ss.equals(" ") ? true : false;
+        }
+
+        return answer;
+    }
+}
